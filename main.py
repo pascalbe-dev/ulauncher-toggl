2e5f60bfd02c6859cdb1a492269f1c54d04bd0c0
Pascal Betting <pascal.betting@gmx.de>
2021-01-17 15:16:50 +0100

    feat: allow starting a new time entry
    + add dialog to select the project

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from ulauncher.api.shared.action.ExtensionCustomAction import \
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 from ulauncher.api.shared.action.RenderResultListAction import \
     RenderResultListAction
+from ulauncher.api.shared.action.SetUserQueryAction import SetUserQueryAction
 from ulauncher.api.shared.event import (ItemEnterEvent, KeywordQueryEvent,
                                         PreferencesEvent,
                                         PreferencesUpdateEvent)
@@ -72,10 +73,13 @@ class TogglExtension(Extension):
         return distinct_time_entries
 
     def start_existing_time_entry(self, time_entry):
+        self.start_new_time_entry(time_entry["pid"], time_entry["description"])
+
+    def start_new_time_entry(self, project_id, description):
         request_payload = {
             "time_entry": {
-                "description": time_entry["description"],
-                "pid": time_entry["pid"],
+                "description": description,
+                "pid": project_id,
                 "created_with": "ULauncher Toggl Extension"
             }
         }
@@ -108,8 +112,9 @@ class TogglExtension(Extension):
         return response
 
     def enrich_with_project(self, time_entry):
-        time_entry.update(
-            {"project": self.get_project_name(time_entry["pid"])})
+        if "pid" in time_entry:
+            time_entry.update(
+                {"project": self.get_project_name(time_entry.get("pid"))})
 
     def enrich_with_current_duration(self, current_time_entry):
         duration_attribute = current_time_entry["duration"]
@@ -132,24 +137,23 @@ class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         query = event.get_argument() or ""
 
-        results = []
-        if query:
-            results = self.gen_existing_time_entries_results(query, extension)
-        else:
-            results = self.gen_current_time_entry_results(extension)
+        if not query:
+            return RenderResultListAction(self.gen_current_time_entry_results(extension))
+        elif str.startswith(query, "p: "):
+            return RenderResultListAction(self.gen_select_project_results(query, extension))
 
-        return RenderResultListAction(results)
+        return RenderResultListAction(self.gen_existing_time_entries_results(query, extension))
 
     def gen_current_time_entry_results(self, extension):
-        timer = extension.get_current_time_entry()
-        if not timer:
+        time_entry = extension.get_current_time_entry()
+        if not time_entry:
             return [self.gen_result_item("No timer running", "Type to start timer.", None)]
 
         stop_action = ExtensionCustomAction(
-            {"action": "stop", "entry_id": timer["id"]})
-        duration = timer["displayable_duration"]
-        description = timer["description"]
-        project = timer["project"]
+            {"action": "stop", "entry_id": time_entry["id"]})
+        duration = time_entry["displayable_duration"]
+        description = time_entry["description"]
+        project = time_entry.get("project", None)
         return [self.gen_result_item(f"{description} | {duration}.",
                                      f"Project: {project}",
                                      stop_action)]
@@ -160,12 +164,36 @@ class KeywordQueryEventListener(EventListener):
             if query.lower() in time_entry["description"].lower()]
 
         if not filtered_existing_time_entries:
-            return [self.gen_result_item("No result found", "Nothing to do here right now.", None)]
+            start_new_action = SetUserQueryAction(
+                f"{extension.keyword} p: {query}")
+            return [self.gen_result_item("No result found", "Select to start new one.", start_new_action)]
 
         return [self.gen_result_item(time_entry["description"],
-                                     "Project: " + time_entry["project"],
+                                     "Project: " +
+                                     str(time_entry.get("project", None)),
                                      ExtensionCustomAction({"action": "start", "entry": time_entry}))
                 for time_entry in filtered_existing_time_entries]
+
+    def gen_select_project_results(self, query, extension):
+        time_entry_description = query[3:]
+
+        no_project_action = ExtensionCustomAction({"action": "start_in_project",
+                                                   "pid": None,
+                                                   "time_entry_description": time_entry_description})
+        no_project_result = self.gen_result_item(
+            "No project", "Select to start time entry without a project.", no_project_action)
+
+        results = [no_project_result]
+        for project_id, project_name in extension.project_map.items():
+            name = project_name
+            description = "Select to start time entry for that project."
+            start_in_project_action = ExtensionCustomAction({"action": "start_in_project",
+                                                             "pid": project_id,
+                                                             "time_entry_description": time_entry_description})
+            results.append(self.gen_result_item(
+                name, description, start_in_project_action))
+
+        return results
 
     def gen_result_item(self, name, description, action):
         action = action or HideWindowAction()
@@ -183,11 +211,19 @@ class ItemEnterEventListener(EventListener):
         elif action == "start":
             time_entry = event_data["entry"]
             extension.start_existing_time_entry(time_entry)
+        elif action == "start_in_project":
+            project_id = event_data["pid"]
+            time_entry_description = event_data["time_entry_description"]
+            extension.start_new_time_entry(project_id, time_entry_description)
+
+            # refill cache to have the new entry in the existing time entries
+            extension.fill_cache()
 
 
 class PreferencesEventListener(EventListener):
     def on_event(self, event, extension):
         extension.token = event.preferences["api_token"]
+        extension.keyword = event.preferences["to"]
         extension.fill_cache()
 
 
@@ -195,6 +231,7 @@ class PreferencesUpdateEventListener(EventListener):
     def on_event(self, event, extension):
         if event.id == "api_token":
             extension.token = event.new_value
+            extension.keyword = event.preferences["to"]
             extension.fill_cache()
 
 
