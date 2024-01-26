6ef862dadcba8115f3ef12e4c6ee03c30de4598b
Pascal Betting <pascal.betting@gmx.de>
2024-01-26 11:10:42 +0100

    feat: support stopping a time entry on api level

--- src/time_entry.py
+++ src/time_entry.py
@@ -1,4 +1,6 @@
 class TimeEntry:
     def __init__(self, api_dto) -> None:
+        self.id = api_dto["id"]
+        self.workspace_id = api_dto["workspace_id"]
         self.description = api_dto["description"] or "<no description>"
         self.is_running = api_dto["stop"] == None
