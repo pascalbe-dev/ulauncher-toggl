3f97b2b05059d5d53d9601ffe5fd9c1f9fffa3dc
Pascal Betting <pascal.betting@gmx.de>
2024-01-26 12:05:31 +0100

    feat: make adding entries work for dynamic workspaces

--- src/toggl_api.py
+++ src/toggl_api.py
@@ -13,6 +13,13 @@ class TogglApi:
     def has_token_set(self):
         return self.token != None and len(self.token) > 31
 
+    def get_workspace_id(self) -> int:
+        response = requests.get(self.base_url + "me", auth=(self.token, "api_token"))
+        if response.status_code != 200:
+            raise Exception("Could not get workspace id: " + response.text)
+
+        return response.json()["default_workspace_id"]
+
     def get_current_time_entry(self) -> TimeEntry:
         response = requests.get(self.base_url + "me/time_entries", auth=(self.token, "api_token"))
         if response.status_code != 200:
@@ -31,15 +38,14 @@ class TogglApi:
             raise Exception("Could not stop time entry: " + response.text)
 
     def start_time_entry(self, description: str) -> None:
-        workspace_id = 2719602
-        creator = "ulauncher-toggl"
+        workspace_id = self.get_workspace_id()
         response = requests.post(self.base_url + "workspaces/" + str(workspace_id) + "/time_entries", auth=(self.token, "api_token"), json={
             "workspace_id": workspace_id,
             "description": description,
-            "created_with": creator,
+            "created_with": "ulauncher-toggl",
             "start": datetime.now(timezone.utc).isoformat(),
             "duration": -1,
-            "tags": ["by:" + creator]
+            "tags": ["by:ulauncher-toggl"]
         })
         if response.status_code != 200:
             raise Exception("Could not start time entry: " + response.text)
